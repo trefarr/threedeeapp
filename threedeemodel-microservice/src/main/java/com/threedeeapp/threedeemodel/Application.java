da5077b60fa72903c120345ca36865d1ce395d36
trevor.farrugia <trevor.farrugia@gmail.com>
2018-03-29 19:22:45 +0200

    threedeeapp first iteration for api and persistence of threedeemodel service

--- threedeemodel-microservice/src/main/java/com/threedeeapp/threedeemodel/Application.java
+++ threedeemodel-microservice/src/main/java/com/threedeeapp/threedeemodel/Application.java
@@ -1,29 +1,42 @@
 package com.threedeeapp.threedeemodel;
 
 
+import com.google.common.base.Predicates;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
 @EnableHystrix
+@EnableSwagger2
 public class Application  {
-
-/*	@Autowired
-	private ModelController com.threedeeapp.threedeemodel.controller;*/
 
 	public Application() {
 	}
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public Docket swaggerSpringMvcPlugin() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.threedeeapp.threedeemodel.controller"))
+				.paths(PathSelectors.any())
+				.build();
 	}
 
 }
